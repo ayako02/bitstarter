fd68a6c093e91cd5100096a3267f8b893c9e5be4
Allison Tu <ayakosky@gmail.com>
2013-07-13 05:20:35 +0000

    update

--- web.js
+++ web.js
@@ -1,12 +1,12 @@
+var express = require('express');
+var app = express.createServer(express.logger());
 var fs = require('fs');
 var content = fs.readFileSync('index.html');
-var express = require('express');
-
-var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+     response.send(content.toString("utf-8"));
 });
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
